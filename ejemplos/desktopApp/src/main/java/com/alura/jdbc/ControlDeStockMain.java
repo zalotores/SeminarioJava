1a3acb0abbb20c9d5103f04b7c0dcb6aac2c5bc4
Horacio Gonzalo Torres <110064757+zalotores@users.noreply.github.com>
2023-06-15 22:26:48 -0300

    Carga de presentacion y ejemplos

---
+++ ejemplos/desktopApp/src/main/java/com/alura/jdbc/ControlDeStockMain.java
@@ -0,0 +1,14 @@
+package com.alura.jdbc;
+
+import javax.swing.JFrame;
+
+import com.alura.jdbc.view.ControlDeStockFrame;
+
+public class ControlDeStockMain {
+
+	public static void main(String[] args) {
+		ControlDeStockFrame productoCategoriaFrame = new ControlDeStockFrame();
+		productoCategoriaFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
